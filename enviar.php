819984dddffb036aba535e5f518f195cf2ae4d53
Jonathan jimenez <106704669+Jonathanjimenez1@users.noreply.github.com>
2023-06-29 12:22:32 +0200

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,22 +1,22 @@
 <?php
-error_reporting(0);
 $nombre = $_POST['nombre'];
-$correo_electronico= $_POST['email'];
+$mail = $_POST['email'];
+$mensaje = $_POST['mensaje'];
 
-$header = 'From: ' . $mail . ", de la poblacion ".$poblacion."\r\n";
+$header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . " \r\n";
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = "Jonathanweb1@outlook.com";
+$para = 'jonathanweb1@outlook.com';
 $asunto = 'Mensaje de mi porfolio';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
-echo 'mensaje enviado correctamente';
-
+header("Location:mensaje.html");
 ?>
